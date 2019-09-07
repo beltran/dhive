5a41d6ee9b45845f9dd6b255fb940beb038efe24
Jaume Marhuenda <jaumemarhuenda@gmail.com>
2019-09-07 09:51:25 -0700

    set hive version 3.1.1 to 3.1.2 (#13)

--- tests/run_tests_travis.sh
+++ tests/run_tests_travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i.bak 's/hive_version.*/hive_version = 3.1.1/g' ./config/*.cfg
-sed -i.bak 's/hadoop_version.*/hadoop_version = 3.1.1/g' ./config/*.cfg
+sed -i.bak 's/hive_version.*/hive_version = 3.1.2/g' ./config/*.cfg
+sed -i.bak 's/hadoop_version.*/hadoop_version = 3.1.2/g' ./config/*.cfg
 sed -i.bak 's/tez_version.*/tez_version = 0.9.1/g' ./config/*.cfg
 sed -i.bak 's/hive_path.*//g' ./config/*.cfg
 sed -i.bak 's/tez_path.*//g' ./config/*.cfg
